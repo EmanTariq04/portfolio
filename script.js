374bbb741b73494e716296f1de07548156e37c8c
Eman Tariq <emantariq.1004@gmail.com>
2025-08-15 22:39:39 +0500

    commit

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ var typed = new Typed("#typed", {
     showCursor: false,
     loop: true
 })
+
+function theme() {
+    const darkBtn = document.getElementById("darkBtn");
+    const lightBtn = document.getElementById("lightBtn");
+
+    document.body.classList.toggle("light");
+
+    if (document.body.classList.contains("light")) {
+        darkBtn.style.display = "block";
+        lightBtn.style.display = "none"
+    } else {
+        darkBtn.style.display = "none";
+        lightBtn.style.display = "block";
+    }
+console.log("theme function is running")
+
+}
